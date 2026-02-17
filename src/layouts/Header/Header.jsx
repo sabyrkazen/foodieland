e3cb85ffa90e2bd6ecae845a70dc887bea8d6993
Sabyrka <sabyrkazen@gmail.com>
2026-02-17 22:51:50 +0500

    feat: implement header component with navigation menu and logo

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,40 @@
 import './Header.scss'
+import Logo from '@/components/Logo'
+import clsx from 'clsx'
 
 export default (props) => {
-  return <header>HEADER</header>
+  const { url } = props
+
+  const menuItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Recipes', href: '/recipes' },
+    { label: 'Blog', href: '/blog' },
+    { label: 'Contact', href: '/contact' },
+    { label: 'About us', href: '/about' },
+  ]
+
+  return (
+    <header className="header">
+      <div className="header__inner inner">
+        <Logo className="header__logo" />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({ label, href }) => (
+              <li
+                key={href}
+                className={clsx(
+                  'header__menu-item',
+                  href === url && 'is-active'
+                )}
+              >
+                <a href={href} className="header__menu-link">
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
