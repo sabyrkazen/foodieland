3bf67a26091c5983d6a748836496f1f98ae39d72
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 16:33:20 +0500

    feat: change AuthorCard name heading from h2 to h3 for semantic improvement

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -5,7 +5,7 @@ export const AuthorCard = ({ className, imgSrc, name, date }) => {
   return (
     <div className={clsx('author-card', className)}>
       <Image className="author-card__image" src={imgSrc} />
-      <h2 className="author-card__name">{name}</h2>
+      <h3 className="author-card__name">{name}</h3>
       <time className="author-card__date" dateTime={date.dateTime}>
         {date.label}
       </time>
