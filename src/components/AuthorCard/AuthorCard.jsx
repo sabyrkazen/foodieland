891315554fe3fd3d5caafd755a3c853cc762db5e
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 22:42:59 +0500

    feat: update AuthorCard layout structure and styles for improved readability

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -6,7 +6,7 @@ export const AuthorCard = ({ className, imgSrc, name, date }) => {
   return (
     <div className={clsx('author-card', className)}>
       <Image className="author-card__image" src={imgSrc} />
-      <div className="author-card__info">
+      <div className="author-card__body">
         <h3 className="author-card__name">{name}</h3>
         <time className="author-card__date" dateTime={date.dateTime}>
           {date.label}
