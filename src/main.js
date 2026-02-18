a099b37d94a3b0df64dde643c580d159110eaef4
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 13:10:06 +0500

    feat: add SliderCollection module for dynamic swiper initialization

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import OverlayMenu from '@/modules/MobileMenu'
+import SliderCollection from '@/modules/SliderCollection'
 
 new OverlayMenu()
+new SliderCollection()
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,7 +3,12 @@ import './Hero.scss'
 export const Hero = () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__slider swiper">
+      <div
+        className="hero__slider swiper"
+        data-js-slider={JSON.stringify({
+          spaceBetween: 40,
+        })}
+      >
         <ul className="hero__slider-list swiper-wrapper">
           <li className="hero__slider-item swiper-slide">Slide 1</li>
           <li className="hero__slider-item swiper-slide">Slide 2</li>
