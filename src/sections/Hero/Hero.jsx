48a10698bff012d2b8c064e987d9a5836d7d42f6
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 13:12:47 +0500

    refactor: simplify Hero component by removing inline slider configuration and deleting SliderCollection module

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,12 +3,7 @@ import './Hero.scss'
 export const Hero = () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div
-        className="hero__slider swiper"
-        data-js-slider={JSON.stringify({
-          spaceBetween: 40,
-        })}
-      >
+      <div className="hero__slider swiper">
         <ul className="hero__slider-list swiper-wrapper">
           <li className="hero__slider-item swiper-slide">Slide 1</li>
           <li className="hero__slider-item swiper-slide">Slide 2</li>
