ecfdf5f5a4d8d2a7d2377965d1be299799ea6692
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 13:20:19 +0500

    feat: add Slider class and SliderCollection module for dynamic swiper initialization

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.scss'
 export const Hero = () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__slider swiper">
+      <div className="hero__slider swiper" data-js-slider="">
         <ul className="hero__slider-list swiper-wrapper">
           <li className="hero__slider-item swiper-slide">
             <div className="hero__card">Slide 1</div>
