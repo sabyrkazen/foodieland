825d17821e11469e1e0beeafe491e1d0950136b0
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 22:37:14 +0500

    feat: refine AuthorCard styles for improved typography and layout consistency

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -7,11 +7,8 @@ export const AuthorCard = ({ className, imgSrc, name, date }) => {
     <div className={clsx('author-card', className)}>
       <Image className="author-card__image" src={imgSrc} />
       <div className="author-card__info">
-        <h3 className="author-card__name default-text">{name}</h3>
-        <time
-          className="author-card__date default-text"
-          dateTime={date.dateTime}
-        >
+        <h3 className="author-card__name">{name}</h3>
+        <time className="author-card__date" dateTime={date.dateTime}>
           {date.label}
         </time>
       </div>
