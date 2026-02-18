3ee9e49203e22bfe0b5485b3818bd44c3f111c05
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 12:54:55 +0500

    feat: implement swiper slider in Hero component for enhanced user experience

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,7 +3,13 @@ import './Hero.scss'
 export const Hero = () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__inner container">Hero section content</div>
+      <div className="hero__slider swiper">
+        <ul className="hero__slider-list swiper-wrapper">
+          <li className="hero__slider-item swiper-slide">Slide 1</li>
+          <li className="hero__slider-item swiper-slide">Slide 2</li>
+          <li className="hero__slider-item swiper-slide">Slide 3</li>
+        </ul>
+      </div>
     </section>
   )
 }
