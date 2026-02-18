dd504bda6dc844260648e3033b2cd8b12020968c
Sabyrka <sabyrkazen@gmail.com>
2026-02-18 13:23:28 +0500

    feat: enhance Slider initialization with dynamic configuration in Hero component

--- src/modules/SliderCollection.js
+++ src/modules/SliderCollection.js
@@ -1,16 +1,20 @@
 import Swiper from 'swiper'
 import 'swiper/css'
+import getAttrNameFromSelector from '@/utils/getAttrNameFromSelector'
 
 const rootSelector = '[data-js-slider]'
 
 class Slider {
   constructor(rootElement) {
     this.rootElement = rootElement
+    this.config = JSON.parse(
+      this.rootElement.getAttribute(getAttrNameFromSelector(rootSelector))
+    )
     this.init()
   }
 
   init() {
-    new Swiper(this.rootElement, {})
+    new Swiper(this.rootElement, this.config)
   }
 }
 
