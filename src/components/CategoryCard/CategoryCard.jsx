91c85ab48c0db136fde4f04d44d5a87c0954baae
Sabyrka <sabyrkazen@gmail.com>
2026-02-21 00:49:26 +0500

    refactor(styles): restructure CategoryCard layout for better modularity
    Encapsulated content under a new `category-card__body` wrapper and replaced absolute positioning with CSS variables for image offset. Improves maintainability and simplifies future updates.

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -4,9 +4,14 @@ import './CategoryCard.scss'
 
 export const CategoryCard = ({ title, imgSrc, gradient }) => {
   return (
-    <article className="category-card" style={{ backgroundImage: gradient }}>
-      <Image className="category-card__image" src={imgSrc} />
-      <h3 className="category-card__title h6">{title}</h3>
+    <article className="category-card">
+      <div
+        className="category-card__body"
+        style={{ backgroundImage: gradient }}
+      >
+        <Image className="category-card__image" src={imgSrc} />
+        <h3 className="category-card__title h6">{title}</h3>
+      </div>
     </article>
   )
 }
