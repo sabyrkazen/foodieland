e6e685f1f8bfc6e039c6908c09ef777a16448354
Sabyrka <sabyrkazen@gmail.com>
2026-02-21 16:15:40 +0500

    refactor(categories): extract category data to separate file
    Centralized category configurations into `categories.data.js` to improve maintainability and reduce duplication in the `Categories` section. Simplifies component logic and streamline imports.

---
+++ src/sections/Categories/categories.data.js
@@ -0,0 +1,45 @@
+import breakfast from '@/assets/images/categories/breakfast.png'
+import vegan from '@/assets/images/categories/vegan.png'
+import meat from '@/assets/images/categories/meat.png'
+import dessert from '@/assets/images/categories/dessert.png'
+import lunch from '@/assets/images/categories/lunch.png'
+import chocolate from '@/assets/images/categories/chocolate.png'
+
+export const categories = [
+  {
+    id: 'breakfast',
+    title: 'Breakfast',
+    imgSrc: breakfast,
+    color: 'rgb(112,130,70)',
+  },
+  {
+    id: 'vegan',
+    title: 'Vegan',
+    imgSrc: vegan,
+    color: 'rgb(108,198,63)',
+  },
+  {
+    id: 'meat',
+    title: 'Meat',
+    imgSrc: meat,
+    color: 'rgb(204,38,27)',
+  },
+  {
+    id: 'dessert',
+    title: 'Dessert',
+    imgSrc: dessert,
+    color: 'rgb(240,158,0)',
+  },
+  {
+    id: 'lunch',
+    title: 'Lunch',
+    imgSrc: lunch,
+    color: 'rgb(0,0,0)',
+  },
+  {
+    id: 'chocolate',
+    title: 'Chocolate',
+    imgSrc: chocolate,
+    color: 'rgb(0,0,0)',
+  },
+]
