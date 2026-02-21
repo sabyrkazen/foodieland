189408daa8504a6a9807ec17a7101262e6e3e290
Sabyrka <sabyrkazen@gmail.com>
2026-02-21 12:54:50 +0500

    feat(categories): add color support for CategoryCard gradient styling
    Introduced a `color` property to the `CategoryCard` component, replacing the previous `gradient` prop. Enhanced SCSS to generate gradients and shadows dynamically using the new `color` variable. Improves maintainability and consistency across category cards.

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -2,12 +2,12 @@ import { Image } from 'minista'
 
 import './CategoryCard.scss'
 
-export const CategoryCard = ({ title, imgSrc, gradient }) => {
+export const CategoryCard = ({ title, imgSrc, color }) => {
   return (
     <article className="category-card">
       <div
         className="category-card__body"
-        style={{ backgroundImage: gradient }}
+        style={{ '--categoryCardColor': `rgb(${color})` }}
       >
         <Image className="category-card__image" src={imgSrc} />
         <h3 className="category-card__title h6">{title}</h3>
