863476904c37ef02c32095b8e4da31547e0b9027
Sabyrka <sabyrkazen@gmail.com>
2026-02-23 18:18:31 +0500

    feat(recipes): add scoped styles for description in Recipes section
    Introduced `Recipes.scss` for section-specific styling and applied a center-aligned `recipes__description` class to the description text. Updated `Recipes.jsx` to import the new styles and link the description with the new className for scoped customization.

--- src/sections/Recipes/Recipes.jsx
+++ src/sections/Recipes/Recipes.jsx
@@ -4,12 +4,15 @@ import { RecipeCard } from '../../components/RecipeCard'
 import { RecipeBanner } from '../../components/RecipeBanner'
 import { recipes } from './recipes.data'
 
+import './Recipes.scss'
+
 export const Recipes = () => {
   return (
     <Section
       title="Simple and tasty recipes"
       titleId="recipes"
       description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+      descriptionClassName="recipes__description"
     >
       <Grid columns={3}>
         {recipes.map((recipe) => {
