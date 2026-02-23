5636f6401b4e94d163eccdc4a8d02e85a00a9ef6
Sabyrka <sabyrkazen@gmail.com>
2026-02-23 17:35:02 +0500

    feat(recipe-banner): implement RecipeBanner component with static content
    Added `RecipeBanner` component with a title, image, and link. Imported and displayed a new static image (`dish.png`). Introduced a lightweight HTML structure and lazy-loaded image for improved performance.

--- src/components/RecipeBanner/RecipeBanner.jsx
+++ src/components/RecipeBanner/RecipeBanner.jsx
@@ -1,3 +1,20 @@
+import dish from '@/assets/images/banner/dish.png'
+
 export const RecipeBanner = () => {
-  return <div>RecipeBanner</div>
+  return (
+    <article className="recipe-banner">
+      <h3 className="recipe-banner__title">Don’t forget to eat healthy food</h3>
+      <img
+        className="recipe-banner__image"
+        src={dish}
+        alt="Healthy dish"
+        width={200}
+        height={200}
+        loading="lazy"
+      />
+      <a className="recipe-banner__link" href="/">
+        www.foodieland.com
+      </a>
+    </article>
+  )
 }
