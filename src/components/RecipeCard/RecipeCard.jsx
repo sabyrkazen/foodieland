102fbdadcf68c8ef8836ea6fc35b4fc3601d795a
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 00:35:59 +0500

    refactor(RecipeCard): replace `<img>` with `<Image>` component for consistency and performance

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,3 +1,5 @@
+import { Image } from 'minista'
+
 import { Tags } from '@components/Tags'
 
 import './RecipeCard.scss'
@@ -7,22 +9,8 @@ export const RecipeCard = ({ title, info, imgSrc, heartIcon }) => {
     <article className="recipe-card">
       <a className="recipe-card__link" href="/">
         <div className="recipe-card__image-wrapper">
-          <img
-            className="recipe-card__image"
-            src={imgSrc}
-            alt=""
-            width={368}
-            height={250}
-            loading="lazy"
-          />
-          <img
-            className="recipe-card__heart"
-            src={heartIcon.imgSrc}
-            alt=""
-            width={48}
-            height={48}
-            loading="lazy"
-          />
+          <Image className="recipe-card__image" src={imgSrc} />
+          <Image className="recipe-card__heart" src={heartIcon.imgSrc} />
         </div>
         <h3 className="recipe-card__title h4">{title}</h3>
         <Tags data={info} />
