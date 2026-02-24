c7c5d273be435f75d2a0ab1077f1c4f5c62a829e
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 00:38:42 +0500

    refactor(RecipeBanner): replace `<img>` with `<Image>` component for consistency and improved performance

--- src/components/RecipeBanner/RecipeBanner.jsx
+++ src/components/RecipeBanner/RecipeBanner.jsx
@@ -1,3 +1,5 @@
+import { Image } from 'minista'
+
 import dish from '@assets/images/recipe-banner/dish.png'
 
 import './RecipeBanner.scss'
@@ -6,15 +8,7 @@ export const RecipeBanner = () => {
   return (
     <article className="recipe-banner">
       <h3 className="recipe-banner__title">Don’t forget to eat healthy food</h3>
-      <img
-        className="recipe-banner__image"
-        src={dish}
-        alt="Healthy dish"
-        title="Healthy dish"
-        width={200}
-        height={200}
-        loading="lazy"
-      />
+      <Image className="recipe-banner__image" src={dish} />
       <a className="recipe-banner__link" href="/">
         www.foodieland.com
       </a>
