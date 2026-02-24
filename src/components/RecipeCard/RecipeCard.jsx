c12cd85ccb44f5d8a7b22a7494d51d0f0cc864d1
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 00:56:38 +0500

    refactor(RecipeCard): add width and height attributes to `<Image>` for improved performance

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -9,8 +9,18 @@ export const RecipeCard = ({ title, info, imgSrc, heartIcon }) => {
     <article className="recipe-card">
       <a className="recipe-card__link" href="/">
         <div className="recipe-card__image-wrapper">
-          <Image className="recipe-card__image" src={imgSrc} />
-          <Image className="recipe-card__heart" src={heartIcon.imgSrc} />
+          <Image
+            className="recipe-card__image"
+            src={imgSrc}
+            width={368}
+            height={250}
+          />
+          <Image
+            className="recipe-card__heart"
+            src={heartIcon.imgSrc}
+            width={48}
+            height={48}
+          />
         </div>
         <h3 className="recipe-card__title h4">{title}</h3>
         <Tags data={info} />
