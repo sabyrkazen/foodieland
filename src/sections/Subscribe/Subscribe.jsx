2f4ad0afd0bf76fb1fdc2ad26d25a0824a829f47
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 14:40:00 +0500

    feat(Subscribe): implement responsive styles and structure for Subscribe section

--- src/sections/Subscribe/Subscribe.jsx
+++ src/sections/Subscribe/Subscribe.jsx
@@ -1,5 +1,35 @@
+import { Button } from '@components/Button'
+
 import './Subscribe.scss'
 
 export const Subscribe = () => {
-  return <div>Subscribe</div>
+  return (
+    <section className="subscribe container">
+      <div className="subscribe__inner">
+        <h2 className="subscribe__title">Deliciousness to your inbox</h2>
+        <div className="subscribe__description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad
+            minim{' '}
+          </p>
+        </div>
+        <form className="subscribe__form">
+          <label className="visually-hidden" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="subscribe__input"
+            id="email"
+            name="email"
+            type="email"
+            placeholder="example@mail.com"
+          />
+          <Button className="subscribe__button" type="submit">
+            Subscribe
+          </Button>
+        </form>
+      </div>
+    </section>
+  )
 }
