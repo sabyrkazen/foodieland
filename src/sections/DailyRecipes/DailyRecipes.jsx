0a38619f02fc4daa86990fe31599090f0c853baa
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 12:38:40 +0500

    feat(DailyRecipes): enhance component with styled layout, dynamic data, and recipe grid

--- src/sections/DailyRecipes/DailyRecipes.jsx
+++ src/sections/DailyRecipes/DailyRecipes.jsx
@@ -1,5 +1,23 @@
+import { Section } from '@layouts/Section'
+import { Grid } from '@components/Grid'
+import { RecipeCard } from '@components/RecipeCard'
+
+import { dailyRecipes } from './daily-recipes.data'
+
 import './DailyRecipes.scss'
 
 export const DailyRecipes = () => {
-  return <div>DailyRecipes</div>
+  return (
+    <Section
+      title="Try this delicious recipe to make your day"
+      titleId="daily-recipes"
+      description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+    >
+      <Grid columns={4}>
+        {dailyRecipes.map((recipe) => (
+          <RecipeCard {...recipe} key={recipe.id} />
+        ))}
+      </Grid>
+    </Section>
+  )
 }
