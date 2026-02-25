a01bf7392eef002989199111bd7ba168400d4672
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 12:51:03 +0500

    feat(DailyRecipeCard): implement new styled component with dynamic props and Tags integration

--- src/components/DailyRecipeCard/DailyRecipeCard.jsx
+++ src/components/DailyRecipeCard/DailyRecipeCard.jsx
@@ -1,5 +1,30 @@
+import { Tags } from '@components/Tags'
+
 import './DailyRecipeCard.scss'
 
-export const DailyRecipeCard = () => {
-  return <div>DailyRecipeCard</div>
+export const DailyRecipeCard = ({ title, info, imgSrc, heartIcon }) => {
+  return (
+    <article className="daily-recipe-card">
+      <a className="daily-recipe-card__link" href="/">
+        <div className="daily-recipe-card__image-wrapper">
+          <img
+            className="daily-recipe-card__image"
+            src={imgSrc}
+            alt=""
+            width={290}
+            height={200}
+          />
+          <img
+            className="daily-recipe-card__heart"
+            src={heartIcon.imgSrc}
+            alt=""
+            width={48}
+            height={48}
+          />
+        </div>
+        <h3 className="daily-recipe-card__title h6">{title}</h3>
+        <Tags data={info} />
+      </a>
+    </article>
+  )
 }
