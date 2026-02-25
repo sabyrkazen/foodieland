1f1d1993a4e2dd914ccba5513140e9cb3bc8d2fc
Sabyrka <sabyrkazen@gmail.com>
2026-02-25 11:41:16 +0500

    feat(InstagramView): enhance section with new layout, styles, and sample posts

--- src/sections/InstagramView/InstagramView.jsx
+++ src/sections/InstagramView/InstagramView.jsx
@@ -1,5 +1,35 @@
+import { Image } from 'minista'
+
+import { Grid } from '@components/Grid'
+import { Section } from '@layouts/Section'
+import { Button } from '@components/Button'
+import { Icon } from '@components/Icon'
+
 import './InstagramView.scss'
 
 export const InstagramView = () => {
-  return <div>InstagramView</div>
+  return (
+    <Section
+      className="instagram-view"
+      title="Check out @foodieland on Instagram"
+      titleId="instagram-view"
+      description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim "
+      isHeaderVertical
+    >
+      <div className="instagram-view__body">
+        <Grid columns={4}>
+          {Array.from({ length: 4 }, (_, index) => (
+            <Image
+              src={`/src/assets/images/instagram-posts/${index + 1}.jpg`}
+              key={index}
+            />
+          ))}
+        </Grid>
+        <Button href="/">
+          Visit Our Instagram
+          <Icon name="instagram" hasFill />
+        </Button>
+      </div>
+    </Section>
+  )
 }
